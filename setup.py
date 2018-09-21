d2eff4b6c11470c7118d2eb5bd4324658022b96a
Jeremi Kaczmarczyk <jeremi.kaczmarczyk@gmail.com>
2018-09-21 13:17:17 +0200

    Fixed url in steup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='harder',
       version='0.0.1',
       description='Reinforcement Learning Utils',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/jknthn/harder',
       author='Jeremi Kaczmarczyk',
       author_email='jeremi.kaczmarczyk@gmail.com',
       license='MIT',
